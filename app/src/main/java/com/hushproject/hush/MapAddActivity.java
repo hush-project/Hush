0687bded9726b0650d827ed7ee83d7e0f2a845e2
johuad <43306406+johuad@users.noreply.github.com>
2019-04-23 22:14:02 -0500

    Minor code adjustment.

--- app/src/main/java/com/hushproject/hush/MapAddActivity.java
+++ app/src/main/java/com/hushproject/hush/MapAddActivity.java
@@ -85,7 +85,6 @@ public class MapAddActivity extends FragmentActivity implements OnMapReadyCallba
 
         final SeekBar setRadius = findViewById(R.id.circleRadius);
 
-        checkPermissions();
         getLastLocation();
 
         mMap.setOnMapClickListener(new GoogleMap.OnMapClickListener() {
@@ -132,35 +131,23 @@ public class MapAddActivity extends FragmentActivity implements OnMapReadyCallba
             }
         });
     }
-    public void checkPermissions() {
-        // Here, thisActivity is the current activity
+
+    public void getLastLocation() {
         if (ContextCompat.checkSelfPermission(this,
                 Manifest.permission.ACCESS_FINE_LOCATION)
                 != PackageManager.PERMISSION_GRANTED) {
 
-            // Permission is not granted
-            // Should we show an explanation?
             if (ActivityCompat.shouldShowRequestPermissionRationale(this,
                     Manifest.permission.ACCESS_FINE_LOCATION)) {
-                // Show an explanation to the user *asynchronously* -- don't block
-                // this thread waiting for the user's response! After the user
-                // sees the explanation, try again to request the permission.
             } else {
-                // No explanation needed; request the permission
                 ActivityCompat.requestPermissions(this,
                         new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
                         MY_PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION);
-
-                // MY_PERMISSIONS_REQUEST_READ_CONTACTS is an
-                // app-defined int constant. The callback method gets the
-                // result of the request.
             }
         } else {
-            // Permission has already been granted
-        }
-    }
 
-    public void getLastLocation() {
+        }
+
         locationManager
                 .requestLocationUpdates(LocationManager.GPS_PROVIDER,
                         1000,
--- app/src/main/java/com/hushproject/hush/MapEditActivity.java
+++ app/src/main/java/com/hushproject/hush/MapEditActivity.java
@@ -88,8 +88,6 @@ public class MapEditActivity extends FragmentActivity implements OnMapReadyCallb
         LatLng circleLatLng = new LatLng(latitude, longitude);
         final SeekBar setRadius = findViewById(R.id.circleRadius);
 
-        checkPermissions();
-
         myCircle = googleMap.addCircle(new CircleOptions()
                 .clickable(true)
                 .center(circleLatLng)
@@ -146,35 +144,22 @@ public class MapEditActivity extends FragmentActivity implements OnMapReadyCallb
             }
         });
     }
-    public void checkPermissions() {
-        // Here, thisActivity is the current activity
+
+    public void zoomSavedLocation() {
         if (ContextCompat.checkSelfPermission(this,
                 Manifest.permission.ACCESS_FINE_LOCATION)
                 != PackageManager.PERMISSION_GRANTED) {
-
-            // Permission is not granted
-            // Should we show an explanation?
             if (ActivityCompat.shouldShowRequestPermissionRationale(this,
                     Manifest.permission.ACCESS_FINE_LOCATION)) {
-                // Show an explanation to the user *asynchronously* -- don't block
-                // this thread waiting for the user's response! After the user
-                // sees the explanation, try again to request the permission.
             } else {
-                // No explanation needed; request the permission
                 ActivityCompat.requestPermissions(this,
                         new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
                         MY_PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION);
-
-                // MY_PERMISSIONS_REQUEST_READ_CONTACTS is an
-                // app-defined int constant. The callback method gets the
-                // result of the request.
             }
         } else {
-            // Permission has already been granted
-        }
-    }
 
-    public void zoomSavedLocation() {
+        }
+
         fusedLocationClient.getLastLocation()
                 .addOnSuccessListener(this, new OnSuccessListener<Location>() {
                     @Override
