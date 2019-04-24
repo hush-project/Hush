f6f9c8e6c2bffb54d1ba6f1a07422b6bab0a6ff0
johuad <43306406+johuad@users.noreply.github.com>
2019-04-23 19:59:14 -0500

    Added a basic custom icon.

--- app/src/main/java/com/hushproject/hush/MapEditActivity.java
+++ app/src/main/java/com/hushproject/hush/MapEditActivity.java
@@ -95,7 +95,7 @@ public class MapEditActivity extends FragmentActivity implements OnMapReadyCallb
                 .center(circleLatLng)
                 .radius(radius)
                 .strokeColor(Color.DKGRAY)
-                .fillColor(0x40D6DBDF));
+                .fillColor(Color.LTGRAY));
 
         zoomSavedLocation();
 
