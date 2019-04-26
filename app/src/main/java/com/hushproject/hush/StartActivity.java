d51e89c6431429fa1ee76d876c1ed6727d34c394
johuad <43306406+johuad@users.noreply.github.com>
2019-04-26 00:52:59 -0500

    Added location notification. Changed permission request code to better request permissions when StartActivity starts.

--- app/src/main/java/com/hushproject/hush/StartActivity.java
+++ app/src/main/java/com/hushproject/hush/StartActivity.java
@@ -1,6 +1,7 @@
 package com.hushproject.hush;
 
 import android.Manifest;
+import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.provider.Settings;
@@ -11,63 +12,43 @@ import android.os.Bundle;
 
 import android.view.View;
 
-public class StartActivity extends AppCompatActivity
-{
-    private static int MY_PERMISSIONS_REQUEST_ACCESS_NOTIFICATION_POLICY = 1;
-    private static int MY_PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION = 1;
-    private static int MY_PERMISSIONS_REQUEST_ACCESS_COARSE_LOCATION = 1;
+public class StartActivity extends AppCompatActivity {
+    //permissions request code.
+    private static int PERMISSIONS = 1;
+
+    //string array for permissions.
+    private final String[] permissions = new String[]{
+            Manifest.permission.ACCESS_FINE_LOCATION,
+            Manifest.permission.ACCESS_COARSE_LOCATION,
+            Manifest.permission.ACCESS_NOTIFICATION_POLICY
+    };
 
     @Override
-    protected void onCreate(Bundle savedInstanceState)
-    {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start);
 
         if (ContextCompat.checkSelfPermission(this,
-                Manifest.permission.ACCESS_NOTIFICATION_POLICY)
-                != PackageManager.PERMISSION_GRANTED) {
-            if (ActivityCompat.shouldShowRequestPermissionRationale(this,
-                    Manifest.permission.ACCESS_NOTIFICATION_POLICY)) {
-            } else {
-                ActivityCompat.requestPermissions(this,
-                        new String[]{Manifest.permission.ACCESS_NOTIFICATION_POLICY},
-                        MY_PERMISSIONS_REQUEST_ACCESS_NOTIFICATION_POLICY);
-            }
-        }
-        else {
-
-        }
-
-        if (ContextCompat.checkSelfPermission(this,
                 Manifest.permission.ACCESS_FINE_LOCATION)
-                != PackageManager.PERMISSION_GRANTED) {
+                    != PackageManager.PERMISSION_GRANTED
+                || ContextCompat.checkSelfPermission(this,
+                Manifest.permission.ACCESS_COARSE_LOCATION)
+                    != PackageManager.PERMISSION_GRANTED
+                || ContextCompat.checkSelfPermission(this,
+                Manifest.permission.ACCESS_NOTIFICATION_POLICY)
+                    != PackageManager.PERMISSION_GRANTED) {
+
             if (ActivityCompat.shouldShowRequestPermissionRationale(this,
                     Manifest.permission.ACCESS_FINE_LOCATION)) {
+
             } else {
                 ActivityCompat.requestPermissions(this,
-                        new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
-                        MY_PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION);
+                        permissions, PERMISSIONS);
             }
         }
         else {
 
         }
-
-        if (ContextCompat.checkSelfPermission(this,
-                Manifest.permission.ACCESS_COARSE_LOCATION)
-                != PackageManager.PERMISSION_GRANTED) {
-            if (ActivityCompat.shouldShowRequestPermissionRationale(this,
-                    Manifest.permission.ACCESS_COARSE_LOCATION)) {
-            } else {
-                ActivityCompat.requestPermissions(this,
-                        new String[]{Manifest.permission.ACCESS_COARSE_LOCATION},
-                        MY_PERMISSIONS_REQUEST_ACCESS_COARSE_LOCATION);
-            }
-        }
-        else {
-
-        }
-
     }
 
     public void toMainActivity(View view)
